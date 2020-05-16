3a1b953fee99c63e41a9b7491a02ba7142d6dd71
unknown <scottdarius03@gmail.com>
2020-05-16 12:07:52 -0400

    added styling to my page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 
-  <title>Bootstrap 4 Website Example</title>
+  <title>Darius' Bootstrap 4 Website</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -16,54 +16,54 @@
   }
   </style>
 </head>
-<body>
+<body style="background-color: WhiteSmoke;">
 
-<div class="jumbotron text-center" style="margin-bottom:0">
+<div class="jumbotron text-center bg-light" style="margin-bottom:0;">
   <h1>My First Bootstrap 4 Page</h1>
   <p><strong>Step 1: Change the background color of this jumbotron</strong></p>
 </div>
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">About Me</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Things I Want to Do</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Things I Want to Be</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Things I Want to Have</a>
       </li>
     </ul>
   </div>
 </nav>
 
-<div class="container" style="margin-top:30px">
+<div class="container" style="margin-top:30px bg-color: MediumPurple;" >
   <div class="row">
     <div class="col-sm-4">
       <h2>About Me</h2>
       <h5>Photo of me:</h5>
-      <div class="fakeimg">Replace with a personal photo</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <div class="thumbnail"><img src="https://media-exp1.licdn.com/dms/image/C4E03AQH5Io60dDBMkw/profile-displayphoto-shrink_200_200/0?e=1594857600&v=beta&t=xS0BmV3RApZk-KQzZmzVjs_oVyuaBhzr6eo7VYGFLeY" size=50%></img></div>
+      <p>I do cool things like make art and skateboard and code.</p>
       <h3>My Favorite Sites</h3>
       <p>Here are a few of my favorite sites.</p>
       <ul class="nav nav-pills flex-column">
         <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
+          <a class="nav-link active" href="https://www.w3schools.com/">W3Schools.com</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="https://github.com/dSc0tt/">Github.com</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="https://www.hackerrank.com/">Hackerrank.com</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+          <a class="nav-link disabled" href="#">Instagram</a>
         </li>
       </ul>
       <hr class="d-sm-none">
@@ -71,19 +71,19 @@
     <div class="col-sm-8">
       <h2>Most Favorite Class</h2>
 
-      <div class="fakeimg">Replace with your favorite class picture</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="thumbnail"><img style="width:30%;" src="https://pbs.twimg.com/profile_images/700543281269092352/Qu2ttYPV_400x400.png"></div>
+      <p>BDPA HSCC Training</p>
+      <p>Of all the classes I take, this one is my favorite. I get to learn to code web applications which can function in the real world.</p>
       <br>
       <h2>Least Favorite Class</h2>
-      <div class="fakeimg">Replace with your least favorite class picture</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <div class="thumbnail"><img style="width:50%;" src="https://p3cdn4static.sharpschool.com/UserFiles/Servers/Server_770743/Image/Technology/Other%20Pictures/math.jpg"></div>
+      <p>Ughh... Math</p>
+      <p>I do well in math, its just boring & there's no real reward. There's no real difference to me in getting the answer right or wrong, its just a number.</p>
     </div>
   </div>
 </div>
 
-<div class="jumbotron text-center" style="margin-bottom:0">
+<div class="jumbotron text-center bg-secondary" style="margin-bottom:0">
   <p><strong>Change the background color of this footer<//p>
 </div>
 
